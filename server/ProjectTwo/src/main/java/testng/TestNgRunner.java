6b5989aa4a3bea068dbe2e89cffb848ce0063099
andrew ahn <andrewsrahn@gmail.com>
2018-05-31 14:38:30 -0400

    modified testngrunner; fixing master

--- server/ProjectTwo/src/main/java/testng/TestNgRunner.java
+++ server/ProjectTwo/src/main/java/testng/TestNgRunner.java
@@ -9,30 +9,60 @@ import org.testng.xml.XmlSuite;
 import org.testng.xml.XmlTest;
 
 public class TestNgRunner {
+//    public static void main(String[] args) {
+//        // Get a reference to an XML Suite
+//        XmlSuite suite = new XmlSuite();
+//        suite.setName("Home Suite");
+//        
+//        // Get a reference to an XML Test
+//        XmlTest test = new XmlTest(suite);
+//        test.setName("Hello World Test");
+//        
+//        // Set the classes for the XML Test
+//        List<XmlClass> classes = new ArrayList<>();
+//        classes.add( new XmlClass("testng.HomeTest") );
+//        classes.add( new XmlClass("testng.ManageTest") );
+//        classes.add( new XmlClass("testng.ManageCreateTest") );
+//        classes.add( new XmlClass("testng.ManageTraineeViewTest") );
+//        test.setXmlClasses(classes);
+//        
+//        // Pass the XML Suite to TestNG
+//        List<XmlSuite> suites = new ArrayList<>();
+//        suites.add(suite);
+//        TestNG testng = new TestNG();
+//        testng.setXmlSuites(suites);
+//        
+//        // Run the TestNG Suite
+//        testng.run();
+//    }
+    
     public static void main(String[] args) {
-        // Get a reference to an XML Suite
-        XmlSuite suite = new XmlSuite();
-        suite.setName("Hello World Suite");
-        
-        // Get a reference to an XML Test
-        XmlTest test = new XmlTest(suite);
-        test.setName("Hello World Test");
-        
-        // Set the classes for the XML Test
-        List<XmlClass> classes = new ArrayList<>();
-        classes.add( new XmlClass("testng.HomeTest") );
-        classes.add( new XmlClass("testng.ManageTest") );
-        classes.add( new XmlClass("testng.ManageCreateTest") );
-        classes.add( new XmlClass("testng.ManageTraineeViewTest") );
-        test.setXmlClasses(classes);
-        
-        // Pass the XML Suite to TestNG
-        List<XmlSuite> suites = new ArrayList<>();
-        suites.add(suite);
-        TestNG testng = new TestNG();
-        testng.setXmlSuites(suites);
-        
-        // Run the TestNG Suite
-        testng.run();
+    	XmlSuite homeSuite = new XmlSuite();
+    	XmlSuite manageSuite = new XmlSuite();
+    	XmlSuite manageCreateSuite = new XmlSuite();
+    	XmlSuite manageTraineeViewSuite = new XmlSuite();
+    	
+    	homeSuite.setName("Home Suite");
+    	manageSuite.setName("Manage Suite");
+    	manageCreateSuite.setName("Manage Create Suite");
+    	manageTraineeViewSuite.setName("Manage Trainee View Suite");
+    	
+    	XmlTest homeTest = new XmlTest(homeSuite);
+    	XmlTest manageTest = new XmlTest(manageSuite);
+    	XmlTest manageCreateTest = new XmlTest(manageCreateSuite);
+    	XmlTest manageTraineeTest = new XmlTest(manageTraineeViewSuite);
+    	
+    	homeTest.setName("Home Test");
+    	
+    	
+    	XmlClass HomeClass = new XmlClass("testng.HomeTest");
+    	XmlClass ManageClass = new XmlClass("testng.ManageTest");
+    	XmlClass ManageCreateClass = new XmlClass("testng.ManageCreateTest");
+    	XmlClass ManageTraineeView = new XmlClass("testng.ManageTraineeViewTest");
+    	
+
+    	
+    	 
+    	
     }
 }
